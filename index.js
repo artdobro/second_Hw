120c69d8655d78290e317d46981553f6a9db1f6b
artdobro <105646581+artdobro@users.noreply.github.com>
2022-11-29 23:28:16 +0200

    add Hw

---
+++ index.js
@@ -0,0 +1,39 @@
+// second Hw
+// 1
+const k1 = '5px'
+const k2 = '12djd334'
+const k3 = '12.45asdwe12'
+const k4 = 'qwqweeewq'
+console.log(Number.parseInt(k1))
+console.log(Number.parseInt(k2))
+console.log(Number.parseInt(k3))
+console.log(Number.parseInt(k4))
+// 2
+console.log(Number.parseFloat(k1))
+console.log(Number.parseFloat(k2))
+console.log(Number.parseFloat(k3))
+console.log(Number.parseFloat(k4))
+// 3
+console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18))
+console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18))
+// 4
+console.log(Math.trunc((Math.random() * (19 - 3) + 3)))
+// 5
+const result = 5 + 5 + '5'
+console.log(result)
+console.log(typeof(result))
+// 6
+const email = 'artem-dobrovolsky@ukr.net'
+console.log(email.indexOf('@'))
+console.log(email.length)
+// 7
+const my = "My"
+const imya = 'name'
+const is = 'is'
+const fullName = my + imya + is +'Viktor'
+console.log(fullName)
+// 8
+const userName = 'Олександра'
+const payment = 300
+const message = 'Дякуемо' + userName + '!' + 'до сплати' + payment + 'гривень!'
+alert(message)
